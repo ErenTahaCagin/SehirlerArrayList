a1d2b6c9589665340db93b97b22ea122ed352810
lab4004 <lab4004@WIN-SERVER>
2022-02-23 11:12:26 +0300

    Add project files.

---
+++ SehirlerArrayList/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SehirlerArrayList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSehir = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnAEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.lbListe = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Şehirler:";
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.Location = new System.Drawing.Point(74, 5);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(140, 20);
+            this.txtSehir.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(15, 31);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnAEkle
+            // 
+            this.btnAEkle.Location = new System.Drawing.Point(96, 31);
+            this.btnAEkle.Name = "btnAEkle";
+            this.btnAEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnAEkle.TabIndex = 3;
+            this.btnAEkle.Text = "Araya Ekle";
+            this.btnAEkle.UseVisualStyleBackColor = true;
+            this.btnAEkle.Click += new System.EventHandler(this.btnAEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(177, 31);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 4;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(96, 60);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 6;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(15, 60);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 5;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(18, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Durum";
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(18, 126);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(0, 20);
+            this.lblDurum.TabIndex = 8;
+            // 
+            // lbListe
+            // 
+            this.lbListe.FormattingEnabled = true;
+            this.lbListe.Location = new System.Drawing.Point(258, 5);
+            this.lbListe.Name = "lbListe";
+            this.lbListe.Size = new System.Drawing.Size(167, 147);
+            this.lbListe.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 160);
+            this.Controls.Add(this.lbListe);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnAEkle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Şehirler";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSehir;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnAEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.ListBox lbListe;
+    }
+}
+
---
+++ SehirlerArrayList/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SehirlerArrayList
+{
+    public partial class Form1 : Form
+    {
+        ArrayList sehirler = new ArrayList();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            sehirler.Add(txtSehir.Text);
+
+            Listele();
+        }
+
+        private void Listele()
+        {
+            lbListe.Items.Clear();
+
+            foreach (string sehir in sehirler)
+            {
+                lbListe.Items.Add(sehir);
+            }
+        }
+
+        private void btnAEkle_Click(object sender, EventArgs e)
+        {
+            int indexNo = lbListe.SelectedIndex;
+            sehirler.Insert(indexNo, txtSehir.Text);
+            Listele();
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            int indexNo = lbListe.SelectedIndex;
+            sehirler[indexNo] = txtSehir.Text;
+            Listele();
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int indexNo = lbListe.SelectedIndex;
+            sehirler.RemoveAt(indexNo);
+            Listele();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            if(sehirler.Contains(txtSehir.Text))
+            {
+                lblDurum.Text = "Aranan Değer Bulundu.";
+            }
+            else
+            {
+                lblDurum.Text = "Aranan Değer Bulunamadı.";
+            }
+        }
+    }
+}
